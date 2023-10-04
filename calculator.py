37607ca3a55732b78aae5975ff03c6eb94b2762c
stuartbiggs <stuartbiggs1973@gmail.com>
2023-10-04 07:53:47 +0100

    Update calculator.py
    additional functions added

--- calculator.py
+++ calculator.py
@@ -7,3 +7,9 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+def Multiply(first_term, second_term):
+    return first_term * second_term
+
+def divide(first_term, second_term):
+    return first_term / second_term
